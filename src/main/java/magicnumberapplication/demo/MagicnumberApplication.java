b5cc86431407a126b0159075f755b2255cb98d29
rodasasfaw <rodasasfaw4@gmail.com>
2018-01-26 14:46:27 -0500

    this is my magic number initial commit

---
+++ src/main/java/magicnumberapplication/demo/MagicnumberApplication.java
@@ -0,0 +1,12 @@
+package magicnumberapplication.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicnumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicnumberApplication.class, args);
+	}
+}
---
+++ src/main/java/magicnumberapplication/demo/Maincontroller.java
@@ -0,0 +1,42 @@
+package magicnumberapplication.demo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Scanner;
+
+@RestController
+
+public class Maincontroller {
+    @RequestMapping("/getnum")
+   public String shownum(@RequestParam("num") int num){
+                    int counter = 0;
+
+                     String magicnum = "";
+                    /* System.out.println("Enter a number:");
+               /*      Scanner keyboard = new Scanner(System.in);
+                     int magic_number = keyboard.nextInt();*/
+
+                     while(num > 1){
+
+                         if(num % 2 == 0)
+                             num/= 2;
+                         else
+                             num = (num * 3) + 1;
+
+                         counter++;
+                         magicnum = magicnum + Integer.toString(num) + "</br> ";
+                         System.out.println(num);
+                         }
+
+
+return "Starting number is: " +magicnum +"</br>";
+                 }
+
+
+
+
+
+
+}
+
